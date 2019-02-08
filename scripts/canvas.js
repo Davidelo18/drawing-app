580ec1ae772baffdc2265b675f731078c6cc5d81
davidelo <daw.kielbik@gmail.com>
2019-02-08 21:01:52 +0100

    simply drawing app

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -3,14 +3,40 @@ window.addEventListener('DOMContentLoaded', () => {
     const myCanvas = document.getElementById('myCanvas');
     const canvContx = myCanvas.getContext('2d');
 
-    // resizing the canvas and adding responsibility to this 
-    resizeCanvas();
-    window.addEventListener('resize', resizeCanvas);
+    // resizing the canvas 
+    myCanvas.height = document.body.clientHeight;
+    myCanvas.width = document.body.clientWidth;
 
-    
+    // bool if user is painting now
+    let radius = 5;
+    let lineWidth = radius * 2;
+    let drawing = false;
+
+    canvContx.lineWidth = lineWidth;
+
+    function startDrawing(e){
+        drawing = true;
+        draw(e);
+    }
+
+    function stopDrawing(){
+        drawing = false;
+        canvContx.beginPath(); // clear the drew lines
+    }
+
+    function draw(e){
+        if(!drawing) return;
+        canvContx.lineTo(e.clientX, e.clientY);
+        canvContx.stroke();
+        canvContx.beginPath();
+        canvContx.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
+        canvContx.fill();
+
+        canvContx.beginPath();
+        canvContx.moveTo(e.clientX, e.clientY);
+    }
+
+    myCanvas.addEventListener('mousedown', startDrawing);
+    myCanvas.addEventListener('mousemove', draw);
+    myCanvas.addEventListener('mouseup', stopDrawing);
 });
-
-function resizeCanvas() {
-    myCanvas.height = window.innerHeight;
-    myCanvas.width = window.innerWidth;
-}
