a1ee26694721cd0ff93767779ebc4988ac14119b
davidelo <daw.kielbik@gmail.com>
2019-02-09 01:09:26 +0100

    added ch background feature

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,34 +1,28 @@
-var mobile;
-function detectmob() {
-    if(navigator.userAgent.match(/Android/i)
-        || navigator.userAgent.match(/webOS/i)
-        || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
-        || navigator.userAgent.match(/iPod/i)
-        || navigator.userAgent.match(/BlackBerry/i)
-        || navigator.userAgent.match(/Windows Phone/i)
-    ){
-        mobile = true;
-    }
-    else {
-        mobile = false;
-    }
-
-    return mobile;
-}
-
 window.addEventListener('DOMContentLoaded', () => {
     // initialize the canvas
     const myCanvas = document.getElementById('myCanvas');
+    const backgroundCanvas = document.getElementById('background');
+    
     const canvContx = myCanvas.getContext('2d');
+    const bgContx = backgroundCanvas.getContext('2d');
 
     // resizing the canvas 
-    myCanvas.height = document.body.clientHeight;
-    myCanvas.width = document.body.clientWidth;
+    myCanvas.height = backgroundCanvas.height = document.body.clientHeight;
+    myCanvas.width = backgroundCanvas.width = document.body.clientWidth;
+
+    bgContx.fillStyle = '#ffffff';
+    bgContx.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
+
+    canvContx.drawImage(backgroundCanvas, 0, 0);
+
+    /* 
+    **  ----- BASIC DRAWING SECTION -----
+    */
 
     // bool if user is painting now
     let radius = 5;
     let lineWidth = radius * 2;
+    let paintColor = '#000000';
     let drawing = false;
 
     canvContx.lineWidth = lineWidth;
@@ -45,9 +39,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function draw(e) {
         if (!drawing) return;
+        canvContx.fillStyle = paintColor;
         canvContx.lineTo(e.clientX, e.clientY);
         canvContx.stroke();
-        canvContx.beginPath();
+        canvContx.beginPath();        
         canvContx.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
         canvContx.fill();
 
@@ -59,11 +54,20 @@ window.addEventListener('DOMContentLoaded', () => {
     myCanvas.addEventListener('mousemove', draw);
     myCanvas.addEventListener('mouseup', stopDrawing);
 
-    if(mobile == true) {
-        myCanvas.addEventListener('touchstart', startDrawing);
-        myCanvas.addEventListener('touchmove', draw);
-        myCanvas.addEventListener('touchend', stopDrawing);
-    }
+    /* 
+    **  ----- MENU OPTIONS SECTION -----
+    */
 
+    const colorToChange = document.getElementById('paintColorChange');
+    const chColorBtn = document.getElementById('chColor');
+    chColorBtn.addEventListener('click', () => {
+        let value = colorToChange.value;
+        if(value.length < 6) return;
+        else if(value.length == 6){
+            bgContx.fillStyle = `#${value}`;
+            bgContx.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
 
+            canvContx.drawImage(backgroundCanvas, 0, 0);
+        }
+    });
 });
